744c4cf2dc3c2c2f12c2c9f694396070f910b19a
Thong <quangthongsainto@gmail.com>
2025-12-22 16:42:11 +0700

    fix homepage

--- GOKCafe.Web/tailwind.config.js
+++ GOKCafe.Web/tailwind.config.js
@@ -2,10 +2,32 @@
 module.exports = {
   content: [
     "./Views/**/*.cshtml",
-    "./wwwroot/**/*.{js,html}",
+    "./wwwroot/**/*.{js,html,css}",
+    "./wwwroot/js/**/*.js",
     "./node_modules/flowbite/**/*.js"
   ],
+
   theme: {
+    // 👇 FIX: Override global breakpoints to remove default 1280px
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1340px',  // Custom breakpoint replacing default 1280px
+      '2xl': '1536px',
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '0rem',
+        lg: '0rem',
+        xl: '0rem',
+        '2xl': '0rem',
+      },
+    },
+
     extend: {
       colors: {
         primary: {
@@ -18,15 +40,18 @@ module.exports = {
           light: '#3ba6e6',
         },
       },
+
       accentColor: {
         primary: '#0767B3',
       },
+
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
         heading: ['Poppins', 'system-ui', 'sans-serif'],
       },
     },
   },
+
   plugins: [
     require('flowbite/plugin')
   ],
